5d2fb7bbfcc15c0720c34d935266e172f1d14c4e
amir-iMac <amir.h.kadivar@gmail.com>
2013-05-10 14:55:22 -0400

    fixed style address and quotation marks

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,18 +1,18 @@
 javascript:
   function run(){
-    if(document.getElementById('gotgreekcss')===null){
-      var style=document.createElement('link');
-      style.id='gotgreekcss';
-      style.type='text/css';
-      style.rel='stylesheet'; 
-      style.href='https://github.com/amirio/got-greek/blob/master/style.css';
+    if(document.getElementById("gotgreekcss")===null){
+      var style=document.createElement("link");
+      style.id="gotgreekcss";
+      style.type="text/css";
+      style.rel="stylesheet"; 
+      style.href="https://s3.amazonaws.com/gotgreek/style.css";
       document.head.appendChild(style);
     }
-    if (document.getElementById('gotgreekjs')===null){
-      var scr=document.body.appendChild(document.createElement('script'));
-      scr.id='gotgreekjs';
-      scr.type ='text/javascript';
-      scr.src='https://github.com/amirio/got-greek/blob/master/gotgreek.js';
+    if (document.getElementById("gotgreekjs")===null){
+      var scr=document.body.appendChild(document.createElement("script"));
+      scr.id="gotgreekjs";
+      scr.type ="text/javascript";
+      scr.src="http://raw.github.com/amirio/got-greek/master/gotgreek.js";
       scr.onreadytstatechange = scr.onload = function(){gotGreek.boot();};
     }else{
       gotGreek.boot();
