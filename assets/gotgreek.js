c56132c969f0bfbfec41f5be65ece4d540ae7b45
Alex Dergachev <alex@evolvingweb.ca>
2014-04-23 00:14:15 -0400

    More refactoring; translates existing selection onload

--- assets/gotgreek.js
+++ assets/gotgreek.js
@@ -1,35 +1,59 @@
 var gotGreek = function(){
 	var	config = {
-			boundaryPattern:/[\s:!.,\"\(\)«»%$]/,
-			nonBoundaryPattern:/[^\s:!.,\"\(\)«»%$]/,
 			googleApiKey:'AIzaSyAICISSmAHfsclKJ4eu5UtbhhtWMLUqxcY',
 			googleTranslateUrl:'https://www.googleapis.com/language/translate/v2',
 			source: '',
 			target: '',
   };
 
-  //external resources that GotGreek has to load
-  var resources= {
-    jQuery: {
-      url: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js',
-      loaded: (typeof jQuery !== 'undefined')
-    },
-    rangyCore: {
-      url: '//rangy.googlecode.com/svn/trunk/dev/rangy-core.js',
-      loaded: (typeof rangy !== 'undefined')
-    }
-  };
   var currentJob = {
     text: '', translation: '', range: null, x:0, y:0
   };
 
   var loaded = false;
-  var running = false;
   var initialized = false;
   var cache, cssApplier;
 
-	// loads all external libraries 
+	// loads all external libraries
 	var load = function(){	
+    //external resources that GotGreek has to load
+    var resources= {
+      jQuery: {
+        url: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js',
+        loaded: (typeof jQuery !== 'undefined')
+      },
+      rangyCore: {
+        url: '//rangy.googlecode.com/svn/trunk/dev/rangy-core.js',
+        loaded: (typeof rangy !== 'undefined')
+      }
+    };
+
+    // checks if all necessary resources are loaded, if so removes the overlay meny and calls boot
+    var terminateLoad= function(){
+      //TODO handle the case were some resources are unavailable
+      var check=true;
+      for (r in resources){
+        check = check && resources[r].loaded;
+      }
+      if (check){
+        loaded = true;
+        jQuery('#gotGreek-menu').text('GotGreek is Ready!');
+        jQuery('#gotGreek-menu').fadeOut(2000,function(){
+          jQuery('#gotGreek-menu').remove();
+        });
+        gotGreek.boot();
+      }
+    };
+
+    var yepnopeCallback= function(url,key,result){
+      for (var r in resources){
+        if (resources[r].url === url){
+          resources[r].loaded = true;
+          return terminateLoad();
+        }
+      }
+    };
+
 		yepnope([{
 			test: resources.jQuery.loaded,
 			nope: resources.jQuery.url,
@@ -40,30 +64,7 @@ var gotGreek = function(){
 			callback: yepnopeCallback
 		}]);
 	};
-	var yepnopeCallback= function(url,key,result){
-		for (var r in resources){
-			if (resources[r].url === url){
-				resources[r].loaded = true;
-				return terminateLoad();
-			}
-		}
-	};
-	// checks if all necessary resources are loaded, if so removes the overlay meny and calls boot
-	var terminateLoad= function(){
-		//TODO handle the case were some resources are unavailable
-		var check=true;
-		for (r in resources){
-			check = check && resources[r].loaded;
-		}
-		if (check){
-			loaded = true;
-			jQuery('#gotGreek-menu').text('GotGreek is Ready!');
-			jQuery('#gotGreek-menu').fadeOut(2000,function(){
-				jQuery('#gotGreek-menu').remove();
-			});
-			gotGreek.boot();
-		}
-	};	
+
 	// initalizes config.source, config.target, and all the state variables
 	var init = function(){
 		//TODO handle the case where html lang is set to an ISO 639-1 non-compliant value
@@ -80,9 +81,10 @@ var gotGreek = function(){
 	};
 
 	var translateListener = function(event){
+    console.log(event);
 
     // only pay attention to left-clicks
-		if (event.button!==0 || !running) {
+		if (event.button!==0) {
       return;
     }
 
@@ -90,22 +92,24 @@ var gotGreek = function(){
     if (rangy.getSelection().isCollapsed){
       if (event.altKey && event.target.nodeName == 'A') {
         // manually select alt-clicked link's text; see http://stackoverflow.com/a/14295222/9621
-        currentJob.range = rangy.createRange();
-        currentJob.range.selectNodeContents(event.target);
-        var sel = rangy.getSelection();
+        var r = rangy.createRange(),
+            sel = rangy.getSelection();
+        r.selectNodeContents(event.target);
         sel.removeAllRanges();
-        sel.addRange(currentJob.range);
-
+        sel.addRange(r);
+        currentJob.range = r;
       }
       else {
         return;
       }
 		}
 		// if there is a selection, push it to its bounding limits
-		else{
-			currentJob.range = rangy.getSelection().getRangeAt(0);
-			pushToLimits(currentJob.range);
+		else {
+      var r = rangy.getSelection().getRangeAt(0);
+			expandToWordBoundary(r);
+			currentJob.range = r;
 		}
+    console.log(currentJob.range);
 
 		if(currentJob.range===null){
 			rangy.getSelection().removeAllRanges();
@@ -117,8 +121,15 @@ var gotGreek = function(){
 		var tmp=currentJob.range.toString();
 		if (typeof tmp !== 'undefined' && /\S/.test(tmp) && /\D/.test(tmp)){
 			currentJob.text = tmp;
-			currentJob.x = event.clientX;
-			currentJob.y = event.clientY;
+      if (event.clientX && event.clientY) {
+        currentJob.x = event.clientX;
+        currentJob.y = event.clientY;
+      }
+      else {
+        var rect = rangy.getSelection().getRangeAt(0).nativeRange.getClientRects()[0];
+        currentJob.x = rect.left;
+        currentJob.y = rect.bottom - 10;
+      }
 			rangy.getSelection().setSingleRange(currentJob.range);
 			if (cache[currentJob.text]){
 				currentJob.translation = cache[currentJob.text];
@@ -129,8 +140,15 @@ var gotGreek = function(){
 				url: config.googleTranslateUrl,
 				type: 'GET',
 				dataType: 'jsonp',
-				success: gotGreek.jsonCallback,
-				error: function(xhr, status){console.log(xhr);console.log(status);},
+				success: function(response){
+          currentJob.translation = response.data.translations[0].translatedText;
+          cache[currentJob.text]=currentJob.translation;
+          showTooltip();
+        },
+        error: function(xhr, status){
+          console.log(xhr);
+          console.log(status);
+        },
 				data: {
 					key: config.googleApiKey,
 					source: config.source,
@@ -141,32 +159,32 @@ var gotGreek = function(){
 		}
 	};
 
-	var showTooltip= function(){
-    jQuery('body').append(
-          jQuery(document.createElement('div'))
-          .attr('id','gotGreek-box')
-          .addClass('gotGreek-box')
-          .html('<p class="translation">'+currentJob.translation+'</p><hr><p class="text">'+currentJob.text+'</p>')
-          .css('top',(jQuery(document).scrollTop()+currentJob.y+10)+'px')
-          .css('left',(jQuery(document).scrollLeft()+currentJob.x+10)+'px'));
-	};
+  var showTooltip= function(){
+    jQuery('<div id="gotGreek-box" class="gotGreek-box">')
+      .html('<p class="translation">'+currentJob.translation+'</p><hr><p class="text">'+currentJob.text+'</p>')
+      .css('top',(jQuery(document).scrollTop()+currentJob.y+10)+'px')
+      .css('left',(jQuery(document).scrollLeft()+currentJob.x+10)+'px')
+      .appendTo('body');
+  };
 
 	// helper function for translateListener, pushes a range to its boundaries
-	var pushToLimits= function(range){
-		var startNodeValue = range.startContainer.nodeValue,
-			endNodeValue = range.endContainer.nodeValue,
-			start= range.startOffset,
-			end = range.endOffset;
-		while (start > 0 && startNodeValue && config.nonBoundaryPattern.test(startNodeValue[start-1])){
-			start--;
-		}
-		while (endNodeValue && end < endNodeValue.length-1 && config.nonBoundaryPattern.test(endNodeValue[end])){
-			end++;
-		}
-		range.setStart(range.startContainer,start);
-		range.setEnd(range.endContainer,end);
-		return range;
-	};
+  var expandToWordBoundary = function(range){
+    var nonBoundaryPattern = /[^\s:!.,\"\(\)«»%$]/,
+        startNodeValue = range.startContainer.nodeValue,
+        endNodeValue = range.endContainer.nodeValue,
+        start= range.startOffset,
+        end = range.endOffset;
+
+    while (start > 0 && startNodeValue && nonBoundaryPattern.test(startNodeValue[start-1])){
+      start--;
+    }
+    while (endNodeValue && end < endNodeValue.length-1 && nonBoundaryPattern.test(endNodeValue[end])){
+      end++;
+    }
+    range.setStart(range.startContainer,start);
+    range.setEnd(range.endContainer,end);
+    return range;
+  };
 
 	/*
 	 * the public interface of gotGreek:
@@ -174,51 +192,38 @@ var gotGreek = function(){
 	return{
 		//this function is the only function that is called from the bookmarklet
 		boot : function(){
-			if(!loaded){
+			if (!loaded) {
 				load();
-			}else if(!initialized){
-				init();
-			}else if(!running){
-				running = true;
-
-        // support alt-clicking on links to translate them
-        jQuery('a').click(function(event){
-          if (event.altKey) {
-            // holding ALT while clicking on a link will prevent navigation
-            event.preventDefault();
-            // but it's still desirable to clear the previous selection
-            rangy.getSelection().removeAllRanges();
-          }
-        });
-
-        // clear result box on any click (mousedown)
-				jQuery('body').mousedown(function(){
-          jQuery('.gotGreek-box').remove();
-				});
-
-        // display translation of selection (mouseup)
-				jQuery('body').mouseup(function(event){
-          // Due to race condition triggered by re-clicking on existing selection,
-          // we need to add a tiny timeout; see https://code.google.com/p/rangy/issues/detail?id=175
-          window.setTimeout(function(){translateListener(event)}, 10);
-        });
-
-			}else if(running){
-				var m = jQuery(document.createElement('div')).attr('id','gotGreek-menu').text('GotGreek is Stopping...');
-				
-				jQuery('body').append(m);
-				jQuery('body').unbind('mouseup',translateListener);
-				running = false;
-				m.fadeOut(1000,function(){
-          jQuery('.gotGreek-box').remove();
-					m.remove();
-				});
+        return;
 			}
-		},
-		jsonCallback : function(response){
-			currentJob.translation = response.data.translations[0].translatedText;
-			cache[currentJob.text]=currentJob.translation;
-			showTooltip();
+      if (!initialized) {
+				init();
+        return;
+			}
+
+      // support alt-clicking on links to translate them
+      jQuery('a').click(function(event){
+        if (event.altKey) {
+          // holding ALT while clicking on a link will prevent navigation
+          event.preventDefault();
+          // but it's still desirable to clear the previous selection
+          rangy.getSelection().removeAllRanges();
+        }
+      });
+
+      // clear result box on any click (mousedown)
+      jQuery('body').mousedown(function(){
+        jQuery('.gotGreek-box').remove();
+      });
+
+      // display translation of selection (mouseup)
+      jQuery('body').mouseup(function(event){
+        // Due to race condition triggered by re-clicking on existing selection,
+        // we need to add a tiny timeout; see https://code.google.com/p/rangy/issues/detail?id=175
+        window.setTimeout(function(){translateListener(event)}, 10);
+      });
+
+      jQuery('body').trigger({ type: 'mouseup', button: 0 });
 		}
 	};
 }();
